c92b4ffdfbb224cce98cb4c72f6400e8f885da9b
Daniel Lee <13857480+Danycraft98@users.noreply.github.com>
2019-07-18 22:53:42 -0400

    updated

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,7 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from argparse import ArgumentParser
-from sqlalchemy import create_engine
 import os
 
 
@@ -16,7 +15,6 @@ class CurrentUser:
 
 
 # "Global" search bar variable
-search_in = ''
 mySQL_con = 'mysql://root:@127.0.0.1/reagent_db'
 host = '127.0.0.1:3307'
 port = '3307'
@@ -34,33 +32,24 @@ app = Flask(__name__)
 current_user = CurrentUser()
 app.config.update({
 	'SECRET_KEY': os.urandom(24),
-	'SQLALCHEMY_DATABASE_URI':'sqlite:///site.db'
-})
-"""'OIDC_CLIENT_SECRETS': 'client_secrets.json',
-	'OIDC_VALID_ISSUERS': ['http://localhost:8080/auth/realms/RK_LIMS'],
-	'OVERWRITE_REDIRECT_URI': 'http://localhost:5000/main',
-	'OIDC_INTROSPECTION_AUTH_METHOD': 'client_secret_post',
-	'OIDC_TOKEN_TYPE_HINT': 'access_token',
-	'OIDC_OPENID_REALM': 'RK_LIMS',
-	'TESTING': True,
-	'DEBUG': True
+	'SQLALCHEMY_DATABASE_URI':'mysql://root:@127.0.0.1/reagent_db'
 })
 
+"""
 if args.dbhost:
 	host = args.dbhost
 	sqluser = os.environ['SQL_USER']
 	sqlpass = os.environ['SQL_PASSWORD']
 	if args.port:
 		port = args.port
-		mySQL_con = 'mysql://' + sqluser + ':' + sqlpass + '@' + host + ':' + port + '/RK_LIMS'
+		mySQL_con = 'mysql://' + sqluser + ':' + sqlpass + '@' + host + ':' + port + '/reagent_db'
 	else:
-		mySQL_con = 'mysql://' + sqluser + ':' + sqlpass + '@' + host + '/RK_LIMS'
+		mySQL_con = 'mysql://' + sqluser + ':' + sqlpass + '@' + host + '/reagent_db'
 """
+
 app.config['SQLALCHEMY_BINDS'] = {'RK_LIMS': mySQL_con}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
-engine_RK = create_engine(mySQL_con)
 db = SQLAlchemy(app)
 
 
