3d5e370d56abc045e5dc73b6e4e1e1cd7c0c5d5d
Daniel Lee <13857480+Danycraft98@users.noreply.github.com>
2019-09-30 14:08:31 -0400

    Moved print options.

--- flask_app/kit_routes.py
+++ flask_app/kit_routes.py
@@ -82,42 +82,7 @@ def add_kit_redirect():
 	comp_lot_nums = request.form.getlist("comp_lot_num")
 	conditions = request.form.getlist("condition")
 
-	kit_label_size = request.form.get('kit_label_size')
-	kit_label = int(request.form.get('kit_label'))
-	comp_label_s = int(request.form.get('comp_label_s'))
-	comp_label_m = int(request.form.get('comp_label_m'))
-
-	batchnum = 1
-	batchpartnum = 1
-	while batchnum <= kit_label:
-		printcont = (request.form.get("name"), exp_date, datetime.now())
-		print_label(printcont, "kit", kit_label_size, None, str(batchpartnum) + '/' + str(kit_label))
-		batchpartnum += 1
-		if batchpartnum > quantity:
-			batchpartnum = 1
-		batchnum += 1
-
 	for name, comp_num, part_num, lot_num, condition in zip(names, comp_nums, comp_part_nums, comp_lot_nums, conditions):
-		batchnum = 1
-		batchpartnum = 1
-		while batchnum <= comp_label_s:
-			printcont = (name, exp_date, datetime.now())
-			print_label(printcont, "kit", "s", None, str(batchpartnum) + '/' + str(comp_label_s))
-			batchpartnum += 1
-			if batchpartnum > quantity:
-				batchpartnum = 1
-			batchnum += 1
-
-		batchnum = 1
-		batchpartnum = 1
-		while batchnum <= comp_label_m:
-			printcont = (name, exp_date, datetime.now())
-			print_label(printcont, "kit", "m", None, str(batchpartnum) + '/' + str(comp_label_m))
-			batchpartnum += 1
-			if batchpartnum > quantity:
-				batchpartnum = 1
-			batchnum += 1
-
 		component = Component(
 			name=name,
 			barcode=comp_num,
@@ -130,3 +95,45 @@ def add_kit_redirect():
 		db.session.commit()
 
 	return redirect(url_for("kits"))
+
+
+@app.route("/print_kit/<int:kit_id>", methods=["GET", "POST"])
+def print_kit(kit_id):
+	kit = Kit.query.filter_by(id=kit_id)[0]
+
+	kit_label_size = request.form.get('kit_label_size')
+	kit_label = int(request.form.get('kit_label'))
+	comp_label_s = int(request.form.get('comp_label_s'))
+	comp_label_m = int(request.form.get('comp_label_m'))
+
+	batchnum = 1
+	batchpartnum = 1
+	while batchnum <= kit_label:
+		printcont = (kit.name, kit.exp_date, kit.date_entered)
+		print_label(printcont, "kit", kit_label_size, None, str(batchpartnum) + '/' + str(kit_label))
+		batchpartnum += 1
+		if batchpartnum > kit.quantity:
+			batchpartnum = 1
+		batchnum += 1
+
+	for component in kit.components:
+		batchnum = 1
+		batchpartnum = 1
+		while batchnum <= comp_label_s:
+			printcont = (component.name, kit.exp_date, kit.date_entered)
+			print_label(printcont, "kit", "s", None, str(batchpartnum) + '/' + str(comp_label_s))
+			batchpartnum += 1
+			if batchpartnum > kit.quantity:
+				batchpartnum = 1
+			batchnum += 1
+
+		batchnum = 1
+		batchpartnum = 1
+		while batchnum <= comp_label_m:
+			printcont = (component.name, kit.exp_date, kit.date_entered)
+			print_label(printcont, "kit", "m", None, str(batchpartnum) + '/' + str(comp_label_m))
+			batchpartnum += 1
+			if batchpartnum > kit.quantity:
+				batchpartnum = 1
+			batchnum += 1
+	return redirect(url_for("kit", kit_id=kit_id))
