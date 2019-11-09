9f26c982acc337530e5137fa61685b3d53733fec
Daniel Lee <13857480+Danycraft98@users.noreply.github.com>
2019-11-08 19:28:21 -0500

    Undone mistakes.

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -33,25 +33,24 @@ app = Flask(__name__)
 current_user = CurrentUser()
 app.config.update({
     'SECRET_KEY': os.urandom(24),
-    # 'SQLALCHEMY_DATABASE_URI':'mysql://irene:irene123@10.0.2.2/reagent_db'
-    'SQLALCHEMY_DATABASE_URI': 'sqlite:///site.db'
+    'SQLALCHEMY_DATABASE_URI':'mysql://irene:irene123@10.0.2.2/reagent_db'
+    # 'SQLALCHEMY_DATABASE_URI': 'sqlite:///site.db'
     # 'SQLALCHEMY_DATABASE_URI':'mysql://root:@localhost/reagent_db'
 })
 
-"""
 if args.dbhost:
-	host = args.dbhost
-	sqluser = os.environ['SQL_USER']
-	sqlpass = os.environ['SQL_PASSWORD']
-	if args.port:
-		port = args.port
-		mySQL_con = 'mysql://' + sqluser + ':' + sqlpass + '@' + host + ':' + port + '/reagent_db'
-	else:
-		mySQL_con = 'mysql://' + sqluser + ':' + sqlpass + '@' + host + '/reagent_db'
+    host = args.dbhost
+    sqluser = os.environ['SQL_USER']
+    sqlpass = os.environ['SQL_PASSWORD']
+    if args.port:
+        port = args.port
+        mySQL_con = 'mysql://' + sqluser + ':' + sqlpass + '@' + host + ':' + port + '/reagent_db'
+    else:
+        mySQL_con = 'mysql://' + sqluser + ':' + sqlpass + '@' + host + '/reagent_db'
 
 app.config['SQLALCHEMY_BINDS'] = {'reagent_db': mySQL_con}
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-"""
+
 db = SQLAlchemy(app)
 
 from flask_app import routes, kit_routes, manufacturer_routes, reagent_routes, made_reagent_routes
