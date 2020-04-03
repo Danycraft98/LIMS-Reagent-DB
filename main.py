0461ffd8a4450470d9990ae73a8b0e425a38c651
Irene Chae <irene.chae@uhn.ca>
2020-04-03 00:23:07 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from app import app
 import os
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', port=os.environ.get('PORT', 5005))
+	app.run(host='0.0.0.0', port='5005') #os.environ.get('PORT', 5005))
