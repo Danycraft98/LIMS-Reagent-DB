fe0fb6617c405033d130e308b5ea74816cddd97f
Irene Chae <jchae2@hotmail.com>
2020-05-22 18:36:28 -0400

    Enhanced the button size and layout.

--- app/__init__.py
+++ app/__init__.py
@@ -20,8 +20,8 @@ args = parser.parse_args()
 app = Flask(__name__)
 app.config.update({
     'SECRET_KEY': os.urandom(24),
-    'SQLALCHEMY_DATABASE_URI':'mysql://irene:irene123@10.0.2.2/reagent_db',
-    # 'SQLALCHEMY_DATABASE_URI':'mysql://root:@localhost/reagent_db',
+    #'SQLALCHEMY_DATABASE_URI':'mysql://irene:irene123@10.0.2.2/reagent_db',
+     'SQLALCHEMY_DATABASE_URI':'mysql://root:@localhost/reagent_db',
     # 'SQLALCHEMY_BINDS': {'reagent_db': mySQL_con},
     'SQLALCHEMY_TRACK_MODIFICATIONS': False
 })
