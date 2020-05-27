d1ddd99a6e242219f7f41f71ca07b57060963732
Irene Chae <jchae2@hotmail.com>
2020-05-27 12:18:49 -0400

    Update function to delete kit with super kit

--- app/route.py
+++ app/route.py
@@ -79,6 +79,8 @@ def delete(element_type, element_id):
         element = SuperKit.query.get(element_id)
     elif element_type == 'kit':
         element = Kit.query.get(element_id)
+        if element.get_super_kit():
+            db.session.delete(element.get_super_kit())
     elif element_type == 'reagent':
         element = Reagent.query.get(element_id)
     elif element_type == 'made_reagent':
