1884250c3a654ee95beecf4a02d3086e8e5b29ba
Irene Chae <jchae2@hotmail.com>
2020-11-23 13:26:37 -0500

    Undone minor mistake.

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ import os
 
 if __name__ == '__main__':
 	current_app = create_app()
-	current_app.run(host='0.0.0.0', port=os.environ.get('PORT', 5005), debug=True)
+	current_app.run(host='0.0.0.0', port=os.environ.get('PORT', 5005)) #, debug=True)
